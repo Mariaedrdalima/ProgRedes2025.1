3532a5d2263d78c921193e5db0241d889c2c3858
Eduarda Lima <maduridade@gmail.com>
2025-04-13 18:10:22 -0300

    Adicionando conversor de base

---
+++ Primeira_Unidade/script_conversor_base10_base6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def conversor(x):
+    resultado = [] #criando a lista que receberá o resultado da conversão
+
+    while x > 0:
+        r = x%6
+        x = x//6
+        resultado.insert(0, r) #com o insert eu consigo dizer a posição onde quero inserir o elemento, assim não preciso inverter a lista no final
+
+    return ''.join(str(s) for s in resultado) #Aqui eu retorno somente a junção dos caracteres da lista
+
+print("******************* BEM VINDO AO CONVERSOR *******************\n")
+
+while True:
+    try:
+        base10 = int(input("Digite o número de base 10: ").replace(" ","")) #removendo todos os espaços que possam ter sido digitados pelo usuário e definindo como int
+        print(f'O número {base10} convertido para base 6 é: {conversor(base10)}')
+        break #encerrando
+
+    #Tratando o erro de tipo caso o user digite caractere invalido    
+    except ValueError:
+        print(f'''
+              ERRO: CARACTERE INVALIDO!
+              Informe um número de base 10 \n''')
+        
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2025.1
 Programação para Redes - CNAT/IFRN - 2025.1 Códigos desenvolvidos durante o curso de Programação para Redes do CNAT/IFRN em 2025.1
+
+
+Unidade 01:
+- Script para converter números da base 10 para base 6;
