31de6a7d0f0d8744c7931faad23122033ac61da9
Eduarda Lima <maduridade@gmail.com>
2025-04-29 20:11:59 -0300

    Update leitor_hex.py

--- Primeira_Unidade/leitor_hex.py
+++ Primeira_Unidade/leitor_hex.py
@@ -6,17 +6,20 @@ dirfile = os.getcwd()
 arqname = 'packipv4.txt'
 arqdir = os.path.join(dirfile,arqname)
 
+with open(arqdir, 'wb') as arq:
+    arq.write(b'\x45\x00\x00\x38\x04\x88\x40\x00\x80\x11\x69\x34\xc0\xa8\x01\x69\xac\xd9\x1e\x0e')
+
 while True:
     try:
-        print(f'Diretorio atual {dirfile}')
-        print(f'Diretorio arq {arqdir}')
-
         with open(arqdir, 'rb') as file:
-            header = file.read(10)
             d = file.read()
-        print(d[0:6])
+        
+        #escrevendo o IP e as demais informações:
+        print(f'ip source:{d[8]}.{d[9]}.{d[10]}.{d[11]}')
+        print(f'flag:{d[6] >> 5}')
+        print(f'offset:{(d[6] & ((1<<5)-1) | d[7])}')
 
         break
 
     except ValueError:
-        print("teste")
+        print("Erro")
