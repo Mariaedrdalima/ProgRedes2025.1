df283802ffd56992122e4a51d2406eaf2835f0ca
Eduarda Lima <maduridade@gmail.com>
2025-05-10 18:45:11 -0300

    Ajustando o fechamento do arquivo e adicionando app1Data

--- Aval01BitsBytes/q3.py
+++ Aval01BitsBytes/q3.py
@@ -11,14 +11,22 @@ dir_atual = os.path.dirname(__file__)
 dir_image = os.path.join(dir_atual, '20190929_175154.jpg')
 
 try:
+    
+
+
     with open(dir_image, 'rb') as f:
+        app1DataSize = 0
+        app1Data = 0
 
         #Pega os primeiros 6 bytes
         header = f.read(6)
 
         # Posições 4 e 5 = Tamanho dos metadados APP1
         app1DataSize = int.from_bytes(header[4:6], byteorder='big')
-        print(f'Tamanho dos metadados (app1DataSize): {app1DataSize}')
+
+        f.close()
+
+    print(f'app1DataSize: {app1DataSize}')
 
 except PermissionError:
     print('Erro: Verifique as permissões de leitura do arquivo.')
