b05f8133b709efb2aea2387b37ab1c0ff5fdf299
mariaedrdalima <maduridade@gmail.com>
2025-06-26 20:19:16 -0300

    Ajustando cliente e server

--- exemplos_professor/client.py
+++ exemplos_professor/client.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
-import socket
-import os
+import socket, os
 
-HOST= "10.25.2.39"
+HOST= "10.25.2.19"
 PORT = 12345
-
-sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sockTCP.connect((HOST, PORT))
-print("Tentando conexão...")
-
-
 
 while True:
     
     try:
+        print("Tentando conexão...")
+        sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockTCP.connect((HOST, PORT))           
+        
         # Convertendo a mensagem digitada de string para bytes
         message = input("Digite a mensagem a ser enviada no teste: ")
         message = message.encode('utf-8')
         sockTCP.send(message)
-        sockTCP.recv()
+        messageback = sockTCP.recv(1024)
+        print(f'Message back: {messageback}')
+        break
 
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
--- exemplos_professor/server.py
+++ exemplos_professor/server.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python3
 
 import socket
-HOST = '10.25.2.39'
+HOST = "10.25.2.19"
 PORT = 12345 #Definindo a porta
 
 
-# Criando o socket TCP
-sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sockTCP.bind((HOST, PORT)) # Ligando o socket a porta
-sockTCP.listen(1) # Máximo de conexões enfileiradas
+
 
 
 while True:
-    con, cliente = sockTCP.accept() # Aceita a conexão com o cliente
-
-    print('Conectado por: ', cliente)
     
     try:
-        #Recebendo
-        message = con.recv(1024) #buffer de 1024 bytes
-        print(f"mensagem recebida: {message.decode('utf-8')}")
+        # Criando o socket TCP
+        print("Escutando...")
+        sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockTCP.bind((HOST, PORT)) # Ligando o socket a porta
+        sockTCP.listen(3) # Máximo de conexões enfileiradas
 
-        if not message: break
-        
-        #Enviando para encerrar
-        print("Enviando mensagem ao cliente...")
-        con.send(message)
-        print('Finalizando Conexão do Cliente ', cliente)
-        con.close()
+        while True:
+            con, cliente = sockTCP.accept() # Aceita a conexão com o cliente
+            print('Conectado por: ', cliente)
+
+            #Recebendo
+            message = con.recv(1024) #buffer de 1024 bytes
+
+            #Se não receber mensagem -> Encerra
+            if not message: break
+            elif message == 'FIM': break
+
+            print(f"mensagem recebida: {message.decode('utf-8')}")
+
+            #Enviando mensagem de volta >>> Encerrando conexão
+            print("Enviando mensagem ao cliente...")
+            con.send(message)
+            print('Finalizando Conexão do Cliente ', cliente)
+            con.close()
         break
-
+        
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
