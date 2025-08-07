6cf2e813d990129415ea0a07406a8a7c1d699a68
mariaedrdalima <maduridade@gmail.com>
2025-08-06 23:34:33 -0300

    Adicionando função get_arq_list pendente flag de tamanho

--- Aval03FileServer/client/client.py
+++ Aval03FileServer/client/client.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 ##########################################>>> IMPORTANDO BIBLIOTECAS E ARQUIVOS ADICIONAIs <<<##########################################
-import socket, threading, os
-from secret import BOT_TOKEN   # Importando o token do bot de um arquivo secreto
-
+import socket, threading, os, struct
+#from secret import BOT_TOKEN   # Importando o token do bot de um arquivo secreto
 
 
 
@@ -14,37 +13,59 @@ SERVER = 'localhost'  # Pode ser alterado para o IP do servidor se necessário
 
 
 
-
 ########################################>>> DEFININDO FUNÇÕES DO CLIENTE/COMUNICAÇÃO COM O SERVER <<<########################################
 # Exibindo menu ao cliente
 def showMenu():
     print("""
-        Confirme qual opção deseja ****
+        Escolha uma opção:
         1 - Listar arquivos do servidor
         2 - Download de 1 arquivo      
         3 - Continuar Download
         4 - Download de vários arquivos
         5 - Sair
           
-        Digite a opção desejada: """, end="")
+        Digite o número da opção desejada: """, end="")
+    
     escolha=int(input())
+
     return escolha
 
 
 #Função para enviar pedido de listagem de arquivos
+
 def get_arq_list():
     sockServer.send(('LIST').encode("utf-8"))
+    arq_list = sockServer.recv(4096).decode()
+    data = arq_list.split("&")
 
-    file_list = sockServer.recv(4096).decode()
-    print("Arquivos disponíveis no servidor:")
-    print(file_list)
+    # Verificando se a resposta do servidor é de sucesso para entender a lista de arquivos
+    if data[0] == "SUCESS":
+        arq_list = data[1].split("\n")
+
+        if arq_list:
+            print("""\nLista de arquivos disponíveis no servidor:""")
+
+            for i, arq in enumerate(arq_list, start=1):
+                print(f"""{i} - {arq}""")
+            print(40* """*""")
+
+            sockServer.close()
+
+        else:
+            print("O servidor não possui arquivos para compartilhar.")
+    else:
+        print(f"ERRO: {data[0]}")
+
+
+
 
 
 
 #Função para enviar pedido de download de arquivo
 def get_down(arq_down):
+
     sockServer.send(('DOWN'+"&"+arq_down).encode("utf-8"))
-    arq_down = sockServer.recv(4096)  # Recebendo confirmação de download
+    arq_down = sockServer.recv(8192)  # Recebendo confirmação de download
 
     # if arq_down:
     #     print(f"Download do arquivo '{arq_down.decode()}' iniciado.")
@@ -65,10 +86,11 @@ def get_down(arq_down):
 
 
 
+
+
 #########################################################>>> INICIANDO CLIENTE <<<#########################################################
 while True:
-
-    # #iniciando o socket do servidor
+    #iniciando o socket do servidor
     sockServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     sockServer.connect((SERVER, PORT)) #Conectando ao servidor
 
@@ -89,8 +111,12 @@ while True:
             print("DOWNLOAD DE VÁRIOS ARQUIVOS")
 
         elif escolha == 5:
-            print("Saindo...")
+            print("\n Saindo...")
             break
+
+
+
+
 
 
 ################################################>>> Sessão de erro para opções inválidas <<<###############################################
@@ -102,8 +128,6 @@ while True:
 
     except KeyboardInterrupt:
         print("\n Saindo do cliente. Crtl+C pressionado.")
-
-
 
 
 
---
+++ Aval03FileServer/server/server.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python3
+
+import socket, threading, struct, os, sys
+
+SERVER = 'localhost'
+PORT = 20000
+allClients = []
+
+
+
+print("Cliente pediu lista de arquivos")
+diretorio = os.path.dirname(os.path.abspath(__file__))
+
+arq_list = os.listdir(os.path.join(diretorio, 'arquivos'))
+#arq_list = ("\n".join(arq_list))
+
+tam = sys.getsizeof(arq_list)
+
+print(arq_list)
+print(tam)
+
+
+
+# ##########################################>>> IMPORTANDO BIBLIOTECAS E ARQUIVOS ADICIONAIs <<<##########################################
+
+def send_arq_list():
+    # Obtendo a lista de arquivos do servidor
+    print("Cliente pediu lista de arquivos")
+    diretorio = os.path.dirname(os.path.abspath(__file__))
+    arq_list = os.listdir(os.path.join(diretorio, 'arquivos'))
+    arq_list = ("\n".join(arq_list))
+
+    print(arq_list)
+
+    
+    # Enviando a lista de arquivos para o cliente
+    sockCon.send(("SUCESS"+"&" + arq_list).encode("utf-8"))
+    print("Lista de arquivos enviada ao cliente.")
+    print()
+    sockCon.close()
+    return 'break'
+
+
+
+
+
+
+# ###################################################>>> TRATANDO SOLICITAÇÕES DO CLIENTE <<<#############################################
+
+def trataCliente(sockCon, origem):
+    print(f"Tratando conexão com {origem}")
+    print()
+
+    while True:
+        req = sockCon.recv(4096)
+        req = req.split(b"&")
+
+        req_cliente = req[0].decode("utf-8")
+
+        if req_cliente == 'LIST':
+            send_arq_list()
+            break
+        
+        elif req_cliente == 'DOWN':
+            arq_down = req[1].decode("utf-8")
+            print(f"Cliente pediu download do arquivo: {arq_down}")
+            sockCon.close()
+            allClients.remove(sockCon)
+            break
+
+        elif req_cliente == 'MULTI':
+            print(f"Cliente pediu download de vários arquivos: {arq_down}")
+            sockCon.close()
+            allClients.remove(sockCon)
+            break 
+
+        elif req_cliente == 'EXIT':
+            print("Cliente pediu para sair")
+            sockCon.close()
+            allClients.remove(sockCon)
+            break
+
+        else:
+            print(f"Requisição desconhecida: {req_cliente}")
+            sockCon.send(("ERRO: Requisição "+req_cliente+" nvalida").encode("utf-8"))
+            sockCon.close()
+            break
+
+            #allClients.remove(sockCon)
+
+        # print (f"Recebi de {origem} -> {msg.decode()}")
+        # sockCon.send()
+        break
+
+
+sockServer = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+sockServer.bind((SERVER, PORT))
+sockServer.listen(5)
+
+while True:
+    print ("Aguardando conexão ...")
+    sockCon, origem = sockServer.accept()
+    trataCliente(sockCon, origem)
+    break
+
+#     #threading.Thread(target=trataCliente, args=(sockCon, origem)).start()
